52a776eb78a35db372772f44b3ecff80e18211a5
Rohit Kumar Barada <rohitbarada@gmail.com>
2025-03-29 17:46:54 +0530

    change of backend and api server

--- client/src/utils/wakeupServer.js
+++ client/src/utils/wakeupServer.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const wakeupServer = async () => {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/wake-up`);
+    const response = await axios.get(`${import.meta.env.VITE_API_URL.replace(/\/api$/, "")}`);
     console.log('Backend server is awake');
     return response.data;
   } catch (error) {
--- server/src/app.js
+++ server/src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.json());
 
 // Add this before your routes
-app.get('/api/wake-up', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Backend is up and running!' });
 });
 
